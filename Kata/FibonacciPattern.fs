d5d0a123d535653904dc2ef1d271b05c3dda615d
maixnor <46966993+maixnor@users.noreply.github.com>
2022-02-18 11:39:17 +0100

    patter matching is amazing - Fibonacci

--- Kata/FibonacciPattern.fs
+++ Kata/FibonacciPattern.fs
@@ -1,18 +1,13 @@
-module Kata.Fibonacci
+module Kata.FibonacciPattern
 
 open Xunit
 open FsUnit.Xunit
 
-let result = (2, 3, true)
+let rec AtIndex n =
+    match n with
+    | 0 | 1 -> n
+    | n -> AtIndex (n-2) + AtIndex (n-1)
 
-let rec atIndex idx previous current =
-    if idx = 0 then
-        previous
-    else
-        atIndex (idx - 1) current (previous + current)
-let AtIndex idx =
-    atIndex idx 0 1
-    
 [<Fact>]
 let ``known index results`` () =
     AtIndex 0 |> should equal 0 
@@ -20,4 +15,4 @@ let ``known index results`` () =
     AtIndex 2 |> should equal 1 
     AtIndex 6 |> should equal 8 
     AtIndex 13 |> should equal 233
-    
+
